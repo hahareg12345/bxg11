8e6d610a2177a9f6430b1addfe402458f0d29a11
wilbert <c@qq.com>
2017-10-18 17:55:46 +0800

    完成添加讲师

--- js/teacher/add.js
+++ js/teacher/add.js
@@ -17,6 +17,33 @@ define([
             //移除模态框容器
             $(this).remove();       //也可以使用：$html.remove();
 
+        }).on("submit","form",function(e){
+
+            //阻止页面刷新
+            e.preventDefault();
+
+            //获取表单数据
+            var formData=$(this).serialize();       //表单都应该具有指定的name属性
+
+            //ajax提交
+            $.ajax({
+                url:"/api/teacher/add",
+                type:"post",
+                data:formData,
+                success:function(res){
+                    if(res.code!=200) throw new Error(res.msg);
+
+                    //隐藏模态框
+                    $html.modal("hide");
+
+
+                    //模拟点击讲师管理按钮，实现刷新讲师列表
+                    $(".list-group a[v=teacher]").trigger("click");
+
+
+                }
+            })
+
         }).appendTo("body").modal();
     }
 })
