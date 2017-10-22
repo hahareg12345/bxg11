de70199622a8b66150e4ba3e3185542e5c226b22
wilbert <c@qq.com>
2017-10-22 15:28:08 +0800

    添加课程-提交表单-刷新课程列表

--- js/course/add.js
+++ js/course/add.js
@@ -10,6 +10,31 @@ define([
 
     return function () {
 
-        $(courseAddTpl).myModal();
+        var $courseAdd=$(courseAddTpl)
+            .on("submit","form",function(e){
+                e.preventDefault();
+
+                //获取表单数据
+                var formData=$(this).serialize();
+
+                //发送ajax请求
+                $.ajax({
+                    url:"/api/course/create",
+                    type:"post",
+                    data:formData,
+                    success:function(res){
+                        if(res.code!=200) throw new Error(res.msg);
+
+                        //隐藏模态框
+                        $courseAdd.modal("hide");
+
+                        //刷新课程列表-->模拟点击课程管理按钮
+                        $(".list-group a[v=course]").trigger("click");
+
+                    }
+                })
+
+            })
+            .myModal();
     }
 })
