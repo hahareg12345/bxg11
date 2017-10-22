8a6966fb3ec8eb127199f5ee3d4c19a6867890fe
wilbert <c@qq.com>
2017-10-22 10:10:03 +0800

    添加分类-下拉框数据编译

--- js/category/add.js
+++ js/category/add.js
@@ -5,19 +5,31 @@
  */
 define([
     "jquery",
-    "text!tpls/categoryAddTpl.html"
-],function($,categoryAddTpl){
+    "text!tpls/categoryAddTpl.html",
+    "art"
+],function($,categoryAddTpl,art){
 
 
     return function(){
 
-        var $categoryAdd=$(categoryAddTpl).myModal();
+        $.ajax({
+            url:"/api/category/top",
+            type:"get",
+            success:function(res){
+                if(res.code!=200) throw new Error(res.msg);
+
+                //将顶级分类的数据提前预置到数据中
+                res.result.unshift({  cg_id:0,cg_name:"顶级分类" });
+
+                //把数据编译到模板中
+                var categoryAdd=art.render(categoryAddTpl,res);
+
+                //把编译成功之后的内容放到页面中
+                var $categoryAdd=$(categoryAdd).myModal();
+            }
+        })
 
 
-        // var $categoryAdd=$(categoryAddTpl).on("hidden.bs.modal",function(){
-        //     //移除模态框容器
-        //     $categoryAdd.remove();
-        //
-        // }).appendTo("body").modal();
+
     }
 })
