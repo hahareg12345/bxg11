fede91e5c753f35fcc0d994fee4d07d0fbddeeaf
wilbert <c@qq.com>
2017-10-22 10:23:44 +0800

    完成添加分类的功能

--- js/category/add.js
+++ js/category/add.js
@@ -25,7 +25,30 @@ define([
                 var categoryAdd=art.render(categoryAddTpl,res);
 
                 //把编译成功之后的内容放到页面中
-                var $categoryAdd=$(categoryAdd).myModal();
+                var $categoryAdd=$(categoryAdd)
+                    .on("submit","form",function(e){
+                        e.preventDefault();
+
+                        //获取表单数据
+                        var formData=$(this).serialize();
+
+                        //发送ajax请求
+                        $.ajax({
+                            url:"/api/category/add",
+                            type:"post",
+                            data:formData,
+                            success:function(res){
+                                if(res.code!=200) throw new Error(res.msg);
+
+                                //关闭模态框-->隐藏模态框
+                                $categoryAdd.modal("hide");
+
+                                //模拟点击课程分类按钮-->从而实现刷新分类列表
+                                $(".list-group a[v=category]").trigger("click");
+                            }
+                        })
+                    })
+                    .myModal();
             }
         })
 
